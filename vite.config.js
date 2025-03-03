700536f25e0f2ec75f9996865d66a992f70255d9
kajalkashyap108 <kashyapkashyap2212@gmail.com>
2025-03-03 10:04:35 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.JPG'], // Include .JPG files as assets
-  server: {
-    mimeTypes: {
-      "application/pdf": ["pdf"]
-    }
-  }
+  
 })
