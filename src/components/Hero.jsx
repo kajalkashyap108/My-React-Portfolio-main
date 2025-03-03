0d255f698deefc1be6ff3b9f7f93b10c21bad26e
kajalkashyap108 <kashyapkashyap2212@gmail.com>
2025-03-03 10:07:16 +0530

    changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { HERO_CONTENT } from '../constants';
-import profilePic from "../assets/hero-1.jpg";
+import profilePic from "../assets/hero-1.JPG";
 import santaHat from "../assets/santahat.png";
 import { motion } from "framer-motion"
 
